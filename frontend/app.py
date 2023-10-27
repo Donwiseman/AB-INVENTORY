a939bed1e3b15e6a768ec27724fcbb118ccbdc64
James Bawa <jbawa032@gmail.com>
2023-10-27 15:46:48 +0100

    added more styles

--- frontend/app.py
+++ frontend/app.py
@@ -3,8 +3,20 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
-def home():
+@app.route('/login.html', strict_slashes=False)
+def login():
     return render_template('login.html')
+
+
+@app.route('/home.html', strict_slashes=False)
+def home():
+    return render_template('home.html')
+
+
+@app.route('/signup.html', strict_slashes=False)
+def signup():
+    return render_template('signup.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
